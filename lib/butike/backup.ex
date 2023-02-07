54471c1c9f6c8988ecad5572d84e318ec993b103
Kamaro <kamaroly@gmail.com>
2023-02-07 20:12:31 +0300

    Added orders retrievial

--- lib/butike/backup.ex
+++ lib/butike/backup.ex
@@ -1,41 +1,47 @@
 defmodule Butike.Backup do
-	
-	@moduledoc """
-	The Backup Context 
-	"""
-	import Ecto.Query
-	alias Butike.Repo
-	alias Ecto.Adapters.SQL
-	alias Butike.Backup.Order
-	alias Butike.Helpers.EnumHelper
+  @moduledoc """
+  The Backup Context
+  """
+  # Imports only from/2 of Ecto.Query
+  import Ecto.Query
+  alias Butike.Repo
+  alias Ecto.Adapters.SQL
+  alias Butike.Backup.Order
+  alias Butike.Helpers.EnumHelper
 
+  @doc """
+  Backups an order
+  """
+  def record(attributes) do
+    # query_values = attributes["query_parameters"]
+    parameters =
+      String.split(attributes["query_parameters"], ",")
+      |> EnumHelper.sanatize()
 
-	@doc """
-	Backups an order
-	"""
-	def record(attributes) do
+    query = attributes["query_string"]
 
-		# query_values = attributes["query_parameters"]
-		parameters = String.split(attributes["query_parameters"], ",")
-					 |> EnumHelper.sanatize()
+    SQL.query!(Repo, query, parameters)
+  end
 
-		query =  attributes["query_string"]
+  def create_order(order) do
+    Repo.insert!(order)
+  end
 
-		SQL.query!(Repo, query, parameters)
-	end
+  def get_orders_by_phone(phone_number) do
+    # Send the query to the repository
+    Repo.all(from(order in Butike.Backup.Order, where: order.shop_msisdn == ^phone_number))
+  end
 
-
-
-	@doc """
-	Returns the list of orders 
-
-	## Examples
-
-		iex> list_orders()
-		[%Order{}, ...]
-
-	"""
-	def list_orders() do
-		Repo.all(from order in Order)
-	end
+  @doc """
+  Returns the list of orders
+  
+  ## Examples
+  
+  	iex> list_orders()
+  	[%Order{}, ...]
+  
+  """
+  def list_orders() do
+    Repo.all(from(order in Butike.Backup.Order))
+  end
 end
--- test/butike_web/controllers/api/backup_controller_test.exs
+++ test/butike_web/controllers/api/backup_controller_test.exs
@@ -1,31 +1,76 @@
 defmodule Butike.BackupControllerTest do
-	
-	use ButikeWeb.ConnCase
-	alias Butike.Backup
+  alias Butike.Backup.Order
+  use ButikeWeb.ConnCase
+  alias Butike.Backup
+  alias Butike.Repo
 
-	describe "Real time backukp" do
+  describe "Real time backukp" do
+    test "App can create real time backup", %{conn: conn} do
+      # Prepare
+      request_payload = %{
+        query_string:
+          "INSERT INTO orders (shop_msisdn,order_type,quantity, item_id, item_name, item_description, customer_or_supplier_id, payment_mode) VALUES (? ,? ,?, ?, ?, ?, ?, ?)",
+        query_parameters: "254757161010,sale,1.72,37,testing_item_name,item description,3,m-pesa"
+      }
 
-		test "App can create real time backup", %{conn: conn} do
-			# Prepare
-			request_payload = %{
-			    query_string: "INSERT INTO orders (shop_msisdn,order_type,quantity, item_id, item_name, item_description, customer_or_supplier_id, payment_mode) VALUES (? ,? ,?, ?, ?, ?, ?, ?)",
-			    query_parameters: "254757161010,sale,1.72,37,testing_item_name,item description,3,m-pesa"
-			}
+      response_payload = %{
+        "code" => 201,
+        "status" => "success",
+        "message" => "Real time backup taken"
+      }
 
-			response_payload = %{
-				"code" => 201,
-				"status" => "success",
-				"message" => "Real time backup taken"
-			}
+      conn = post(conn, Routes.api_v1_backup_real_time_path(conn, :real_time, request_payload))
 
-			conn = post(conn, Routes.api_v1_backup_real_time_path(conn, :real_time, request_payload))
-			
-			# Ensure the response is correct
-			assert json_response(conn, 200) == response_payload
-			
-			# Ensure that the data base been persisted in the database
-			orders = Backup.list_orders()
-			assert Enum.count(orders) == 1
-		end
-	end
+      # Ensure the response is correct
+      assert json_response(conn, 200) == response_payload
+
+      # Ensure that the data base been persisted in the database
+      orders = Backup.list_orders()
+      assert Enum.count(orders) == 1
+    end
+
+    test "User can retrieve orders by shop phone" do
+      # 1. Create user in the database
+      order = %Order{
+        shop_msisdn: "250781854852",
+        order_type: "purchase",
+        item_id: 43,
+        item_name: "Sale Item",
+        item_description: "Why is this even a sale",
+        customer_or_supplier_id: 1,
+        quantity: 43,
+        cost_price: 2,
+        sale_price: 3,
+        payment_mode: "m-pesa"
+      }
+
+      Backup.create_order(order)
+      Backup.create_order(order)
+
+      stored_orders = Backup.get_orders_by_phone(order.shop_msisdn)
+
+      assert Enum.count(stored_orders) == 2
+    end
+
+    test "get_orders_by_phone returns 0 results when there is no matching phone" do
+      order = %Order{
+        shop_msisdn: "250781854852",
+        order_type: "purchase",
+        item_id: 43,
+        item_name: "Sale Item",
+        item_description: "Why is this even a sale",
+        customer_or_supplier_id: 1,
+        quantity: 43,
+        cost_price: 2,
+        sale_price: 3,
+        payment_mode: "m-pesa"
+      }
+
+      Backup.create_order(order)
+
+      empty_orders = Backup.get_orders_by_phone("2547575772")
+
+      assert Enum.count(empty_orders) == 0
+    end
+  end
 end
