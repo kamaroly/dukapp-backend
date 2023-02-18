510269888f9c8b52b8de76d5d79c4256a558b4c7
Kamaro <kamaroly@gmail.com>
2023-02-18 12:14:49 +0300

    Added test to cover up all the tests

--- test/butike_web/controllers/api/backup_controller_test.exs
+++ test/butike_web/controllers/api/backup_controller_test.exs
@@ -1,6 +1,11 @@
 defmodule Butike.BackupControllerTest do
   alias Butike.UserService
   alias Butike.Order.Order
+  alias Butike.Order.OrderItem
+  alias Butike.Item.Item
+  alias Butike.Item.Inventory
+  alias Butike.Customer.Customer
+  alias Butike.Supplier.Supplier
   use ButikeWeb.ConnCase
   alias Butike.Backup.Backup
   alias Butike.Repo
@@ -29,6 +34,105 @@ defmodule Butike.BackupControllerTest do
       # Ensure that the data base been persisted in the database
       orders = Repo.all(Order)
       assert Enum.count(orders) == 1
+    end
+
+    test "Mobile phone can backup order items", %{conn: conn} do
+      request_payload = %{
+        query_string:
+          "INSERT INTO order_items (shop_msisdn,name,item_id,order_id,description,variation_id,quantity,unit_cost_price,unit_sales_price,total,taxes,note,meta_data) VALUES (?,?,?,?,?,?,?,?,?,?,?,?,?)",
+        query_parameters:
+          "250781854852,Sale Item,123,1,Order description,0,10.0,7,8,70.0,0,Test note,[]"
+      }
+
+      conn = post(conn, Routes.api_v1_backup_real_time_path(conn, :real_time, request_payload))
+
+      assert json_response(conn, 200) == %{
+               "code" => 200,
+               "status" => "success",
+               "message" => "Real time backup taken"
+             }
+
+      order_items = Repo.all(OrderItem)
+      assert Enum.count(order_items) == 1
+    end
+
+    test "Mobile phone can backup items", %{conn: conn} do
+      request_payload = %{
+        query_string:
+          "INSERT INTO items (shop_msisdn,shop_item_id,name,description,category,reorder_level,quantity,cost_price,sale_price,is_service,note,meta_data) VALUES (?,?,?,?,?,?,?,?,?,?,?,?)",
+        query_parameters:
+          "250781854852,2,Item Name,Item Description,1,49,90,43,23,97,well written note,another test"
+      }
+
+      conn = post(conn, Routes.api_v1_backup_real_time_path(conn, :real_time, request_payload))
+
+      assert json_response(conn, 200) == %{
+               "code" => 200,
+               "status" => "success",
+               "message" => "Real time backup taken"
+             }
+
+      items = Repo.all(Item)
+      assert Enum.count(items) == 1
+    end
+
+    test "Mobile phone can backup item inventory", %{conn: conn} do
+      request_payload = %{
+        query_string:
+          "INSERT INTO item_inventories (shop_msisdn,item_id,comment,quantity,amount) VALUES (?,?,?,?,?)",
+        query_parameters: "250781854852,1,New stock,32,300"
+      }
+
+      conn = post(conn, Routes.api_v1_backup_real_time_path(conn, :real_time, request_payload))
+
+      assert json_response(conn, 200) == %{
+               "code" => 200,
+               "status" => "success",
+               "message" => "Real time backup taken"
+             }
+
+      item_inventories = Repo.all(Inventory)
+      assert Enum.count(item_inventories) == 1
+    end
+
+    test "Mobile phone can backup customers", %{conn: conn} do
+      request_payload = %{
+        query_string:
+          "INSERT INTO customers (address,email,meta_data,names,note,phone,shop_customer_id,shop_msisdn) VALUES (?,?,?,?,?,?,?,?)",
+        query_parameters:
+          "Gikondo Kicukiro,customer@remote.com,[],John Remote,Customer Well Note,250781854852,1,250781854852"
+      }
+
+      conn = post(conn, Routes.api_v1_backup_real_time_path(conn, :real_time, request_payload))
+
+      assert json_response(conn, 200) == %{
+               "code" => 200,
+               "status" => "success",
+               "message" => "Real time backup taken"
+             }
+
+      customers = Repo.all(Customer)
+      assert Enum.count(customers) == 1
+    end
+
+    test "Mobile phone can backup suppliers", %{conn: conn} do
+      request_payload = %{
+        query_string:
+          "INSERT INTO suppliers (address,company_name,email,meta_data,note,phone,shop_msisdn,shop_supplier_id,tin) VALUES (?,?,?,?,?,?,?,?,?)",
+        query_parameters:
+          "Nyenyeri Gikondo Kicukiro Kigali,DukApp Industry,suppliers@remote.com,[],Supplier Note,254757161010,250781854852,1,100594839"
+      }
+
+      conn = post(conn, Routes.api_v1_backup_real_time_path(conn, :real_time, request_payload))
+
+      assert json_response(conn, 200) == %{
+               "code" => 200,
+               "status" => "success",
+               "message" => "Real time backup taken"
+             }
+
+      suppliers = Repo.all(Supplier)
+      assert Enum.count(suppliers) == 1
     end
 
     test "User can restore the backup by shop phone number", %{conn: conn} do
